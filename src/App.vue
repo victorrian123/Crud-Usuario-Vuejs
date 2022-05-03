3de713dc5ce303b543570b1b2e2d23e7d397c098
Victor Rian <victor.rian@hotmail.com>
2022-05-03 20:39:07 -0300

    Salvar, Editar, Excluir, Crud completo, utilizando VueJS

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<template>
+	<div id="app" class="container">
+		<h1>HTTP com Axios(Cadastro)</h1>
+		<b-alert show dismissible v-for="mensagem in mensagens" :key="mensagem.texto"
+		:variant="mensagem.tipo">{{ mensagem.texto }}</b-alert>
+		<b-card>
+			<b-form-group label="Nome:">
+				<b-form-input type="text" size="lg"
+				v-model="usuario.nome"
+				placeholder="Informe o Nome"></b-form-input>
+			</b-form-group>
+			<b-form-group label="Email:">
+				<b-form-input type="email" size="lg"
+				v-model="usuario.email"
+				placeholder="Informe o Email"></b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button @click="salvar" size="lg" variant="success">Salvar</b-button>
+			<b-button @click="obterUsuarios" size="lg" class="ml-4" variant="primary">Mostrar Usuarios</b-button>
+		</b-card>
+		<hr>
+		<b-list-group>
+			<b-list-group-item v-for="(usuario, id) in usuarios" key="id">
+				<strong>Nome: </strong>{{ usuario.nome }}<br>
+				<strong>Email: </strong>{{ usuario.email }}<br>
+				<strong>ID: </strong>{{ id }}<br>
+				<b-button variant="warning" size="lg"
+				@click="carregar(id)">Editar</b-button>
+				<b-button variant="danger" size="lg" class="ml-3" @click="excluir(id)">Excluir</b-button>
+			</b-list-group-item>
+		</b-list-group>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			mensagens: [],
+			usuarios: [],
+			id: null,
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		limpar() {
+			this.usuario.nome = ''
+			this.usuario.email = ''
+			this.id = null
+			this.mensagens = []
+		},
+		salvar() {
+			const metodo = this.id ? 'patch' : 'post'
+			const finalUrl = this.id ? `/${this.id}.json` : '.json'
+			this.$http[metodo](`/usuarios${finalUrl}`, this.usuario)
+			.then(() => {
+				this.limpar()
+				this.mensagens.push({
+					texto: 'Salvo com Sucesso!!!',
+					tipo: "success"
+				})
+			})
+		},
+		obterUsuarios() {
+			this.$http.get('usuarios.json').then(res => {
+				this.usuarios = res.data
+			})
+		},
+		carregar(id) {
+			this.id = id
+			this.usuario = {...this.usuarios[id]}
+		},
+		excluir(id) {
+			this.$http.delete(`/usuarios/${id}.json`)
+			.then(() => {
+				this.limpar()
+				this.mensagens.push({
+					texto: 'Excluido com Sucesso!!!',
+					tipo: "danger"
+				})
+			})
+		}
+	}
+
+
+	// created() {
+	// 	this.$http.post('usuarios.json', {
+	// 		nome: 'Victor',
+	// 		email: 'Victor@victor.com'
+	// 	}).then(res => console.log(res))
+	// }
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	font-size: 1.5rem;
+}
+
+#app h1 {
+	text-align: center;
+	margin: 50px;
+}
+</style>
